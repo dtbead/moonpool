1cc7bfdf18ee118683ef4f9577503f64437b116d
dtbead <113726875+dtbead@users.noreply.github.com>
2024-03-29 12:22:27 -0500

    add test forGetHashes

--- file/file_test.go
+++ file/file_test.go
@@ -6,7 +6,10 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"reflect"
 	"testing"
+
+	"github.com/dtbead/moonpool/media"
 )
 
 type mockFile struct {
@@ -83,6 +86,36 @@ func Test_copy(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if err := copy(tt.args.destination, tt.args.r); (err != nil) != tt.wantErr {
 				t.Errorf("copy() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestGetHashes(t *testing.T) {
+	type args struct {
+		r io.Reader
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    media.Hashes
+		wantErr bool
+	}{
+		{"valid", args{testFile.f}, media.Hashes{
+			MD5:    []byte{212, 29, 140, 217, 143, 0, 178, 4, 233, 128, 9, 152, 236, 248, 66, 126},
+			SHA1:   []byte{218, 57, 163, 238, 94, 107, 75, 13, 50, 85, 191, 239, 149, 96, 24, 144, 175, 216, 7, 9},
+			SHA256: []byte{227, 176, 196, 66, 152, 252, 28, 20, 154, 251, 244, 200, 153, 111, 185, 36, 39, 174, 65, 228, 100, 155, 147, 76, 164, 149, 153, 27, 120, 82, 184, 85},
+		}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetHashes(tt.args.r)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetHashes() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetHashes() = %v, want %v", got, tt.want)
 			}
 		})
 	}
