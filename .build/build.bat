73e062280c1f6eedd8148d2545219b7a3a1bafcc
dtbead <113726875+dtbead@users.noreply.github.com>
2024-11-05 10:42:08 -0600

    update build.bat
    included missing tailwindcss settings

--- .build/build.bat
+++ .build/build.bat
@@ -1,4 +1,6 @@
 sqlc generate -f "internal/db/sqlc_archive.yaml"
 sqlc generate -f "internal/db/sqlc_thumbnail.yaml"
 
+tailwindcss-windows-x64.exe -c "internal/www/tailwind.config.js" -i "internal/www/assets/static/style.css" -o "internal/www/assets/static/tailwind.css"
+
 go build -C "cmd/moonpool" -a -installsuffix cgo -ldflags '-s'
