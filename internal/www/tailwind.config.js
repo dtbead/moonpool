1166bab5284a314c8df52f9349d79a94efbcbb1b
dtbead <113726875+dtbead@users.noreply.github.com>
2024-11-29 19:32:04 -0600

    [webui] changed some css styling for left_sidebar search

--- internal/www/tailwind.config.js
+++ internal/www/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
@@ -7,7 +9,20 @@ module.exports = {
     theme: {
         colors: {
             transparent: 'transparent',
-            main: "#27208e",
+            white: "white",
+            main: {  
+                '50': '#ebf0ff',
+                '100': '#dbe3ff',
+                '200': '#becbff',
+                '300': '#97a8ff',
+                '400': '#6d78ff',
+                '500': '#4c4bff',
+                '600': '#3e2cff',
+                '700': '#3320e2',
+                '800': '#2a1db6',
+                '900': '#27208e',
+                '950': '#191353',
+            },
             main_darker: "#1b2065",
             second: "#aea8ba",
             third: "#7a7485",
