f6dc97ef7685fe3cc61cbb46eeb824f2a0befad7
dtbead <113726875+dtbead@users.noreply.github.com>
2024-12-05 17:19:39 -0600

    [internal/db] renamed sqlc config files

--- .build/build.bat
+++ .build/build.bat
@@ -1,5 +1,5 @@
-sqlc generate -f "internal/db/sqlc_archive.yaml"
-sqlc generate -f "internal/db/sqlc_thumbnail.yaml"
+sqlc generate -f "internal/db/archive_sqlc.yaml"
+sqlc generate -f "internal/db/thumbnail_sqlc.yaml"
 
 tailwindcss-windows-x64.exe -c "internal/www/tailwind.config.js" -i "internal/www/assets/static/style.css" -o "internal/www/assets/static/tailwind.css"
 
