dtbead/moonpool

commit e14b30ab9f924170e99ced288fa74d313d074cc1

[internal/db] added WITHOUT ROWID optimization to tags_alias table

internal/db/archive_schema.sql
@@ -49,7 +49,7 @@ CREATE TABLE tags_alias (
 	"text"		TEXT NOT NULL,
 	FOREIGN KEY("tag_id") REFERENCES "tags"("tag_id") ON DELETE CASCADE, 
 	UNIQUE (tag_id, text) ON CONFLICT IGNORE
-);
+) WITHOUT ROWID;
 
 CREATE TABLE tag_map (
 	"tag_id"	INTEGER NOT NULL,

commit da162dc18fa9850d1840ea5a5eccdaf9e2ef6bc7

[internal/db] fixed sql schema
caused by commit e14b30ab9f924170e99ced288fa74d313d074cc1

internal/db/archive_schema.sql
@@ -45,7 +45,7 @@ CREATE TABLE tags (
 );
 
 CREATE TABLE tags_alias (
-	"tag_id"	INTEGER NOT NULL,
+	"tag_id"	INTEGER NOT NULL PRIMARY KEY,
 	"text"		TEXT NOT NULL,
 	FOREIGN KEY("tag_id") REFERENCES "tags"("tag_id") ON DELETE CASCADE, 
 	UNIQUE (tag_id, text) ON CONFLICT IGNORE