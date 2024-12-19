dtbead/moonpool

commit 7243591cf7120cc95db8614724d95db60ef45f5c

[webui/internal] moved templates & static files into its own folder

internal/www/www.go
@@ -3,6 +3,7 @@ package www
 import (
 	"context"
 	"embed"
+	"fmt"
 	"html/template"
 	"io"
 	"log/slog"
@@ -15,11 +16,8 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-//go:embed assets
-var folderAssets embed.FS
-
-//go:embed templates
-var folderTemplates embed.FS
+//go:embed web/**
+var webFolder embed.FS
 
 type WWW struct {
 	echo   *echo.Echo
@@ -74,10 +72,15 @@ func (w WWW) Start(ListenAddress string) error {
 	if w.config.DynamicWebReloading {
 		w.echo.Static("/", w.config.DynamicWebReloadingPath)
 	} else {
-		w.echo.StaticFS("/", folderAssets)
+		w.echo.StaticFS("/", webFolder)
 
 		t := template.New("").Funcs(templateFuncMap)
-		t, err := t.ParseFS(folderTemplates, "templates/*")
+		t, err := t.ParseFS(webFolder, "web/templates/*")
+		if err != nil {
+			return err
+		}
+
+		t, err = t.ParseFS(webFolder, "web/assets/**")
 		if err != nil {
 			return err
 		}
@@ -85,6 +88,22 @@ func (w WWW) Start(ListenAddress string) error {
 		w.echo.Renderer = &Template{t}
 	}
 
+	fA, err := webFolder.ReadDir("web/templates")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fT, err := webFolder.ReadDir("web/assets/scripts")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	for _, v := range fA {
+		fmt.Println(v.Name())
+	}
+	for _, v := range fT {
+		fmt.Println(v.Name())
+	}
 	return w.echo.Start(ListenAddress)
 }
 

internal/www/thumbnail.go
@@ -8,7 +8,7 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-//go:embed assets/static/404.png
+//go:embed web/assets/static/404.png
 var defaultThumbnailPNG []byte
 
 func (w WWW) Thumbnail() {

commit 074067b965e8c05a4159af995dfa4372d6336c0e

[webui] fixed assets not loading (caused by 7243591cf7120cc95db8614724d95db60ef45f5c)

internal/www/www.go
@@ -15,8 +15,11 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 )
 
-//go:embed web/**
-var webFolder embed.FS
+//go:embed web/templates/*
+var webFolderTemplates embed.FS
+
+//go:embed web/assets/**
+var webFolderAssets embed.FS
 
 type WWW struct {
 	echo   *echo.Echo
@@ -71,15 +74,10 @@ func (w WWW) Start(ListenAddress string) error {
 	if w.config.DynamicWebReloading {
 		w.echo.Static("/", w.config.DynamicWebReloadingPath)
 	} else {
-		w.echo.StaticFS("/", webFolder)
+		w.echo.StaticFS("/assets", echo.MustSubFS(webFolderAssets, "web/assets"))
 
 		t := template.New("").Funcs(templateFuncMap)
-		t, err := t.ParseFS(webFolder, "web/templates/*")
-		if err != nil {
-			return err
-		}
-
-		t, err = t.ParseFS(webFolder, "web/assets/**")
+		t, err := t.ParseFS(webFolderTemplates, "web/templates/*")
 		if err != nil {
 			return err
 		}